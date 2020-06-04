34b0db6127d2ab6ee8c21b2ffaa8ae58d76761d7
Max Hopf <Markus.Hopf.Extern@fiduciagad.de>
2020-06-04 15:44:53 +0200

    python test.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PythonApp
 Python App
+
+## Run
+
+python test.py
+
---
+++ test.py
@@ -0,0 +1,7 @@
+print "Hello, Python!"
+if True:
+    print "Answer"
+    print "True"
+else:
+    print "Answer"
+    print "False"
