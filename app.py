1850f2dc057976eda0dcb386c70cc067663aa792
Hefezopf <markus@hopf-it.de>
2020-06-05 10:50:58 +0200

    min

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
 #!/usr/bin/python
 
-	# public static void main(String[] args) {
-	# 	System.out.println("Start App...");
-
-	# 	//String[] coords = { "(1,1)", "(1,3)", "(4,4)", "(6,1)" };
-	# 	Point[] points;
-	# 	if (args.length > 0) {
-	# 		points = PointUtil.parseAllCoords(args);
-	# 	} else {
-	# 		Point[] pointsFixed = new Point[4];
-	# 		pointsFixed[0] = new Point(1, 1);
-	# 		pointsFixed[1] = new Point(1, 3);
-	# 		pointsFixed[2] = new Point(4, 4);
-	# 		pointsFixed[3] = new Point(6, 1);
-	# 		points = pointsFixed;
-	# 	}
-
-	# 	double minDistanceOfArray = PointUtil.determineMinimumOfArray(points);
-	# 	System.out.println("Minimum distance of all points = " + minDistanceOfArray);
-	# 	double maxDistanceOfArray = PointUtil.determineMaximumOfArray(points);
-	# 	System.out.println("Maximum distance of all points = " + maxDistanceOfArray);
-	# }
-
-# def printme( str ):
-#    print (str)
-#    return    
-
 import sys
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+def parseAllCoords(args):
+   return [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
+
+def determineMinimumOfArray(points):
+   return 2.0
+
+def determineMaximumOfArray(points):
+   return 5.39
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 print("Start App...")
 
-minDistanceOfArray = 9
-maxDistanceOfArray = 99
-print ("Minimum distance of all points = ", str(minDistanceOfArray))
-print ("Maximum distance of all points = ", str(maxDistanceOfArray))
+if len(sys.argv) > 1:
+  points = parseAllCoords(sys.argv)
+else:
+  points = [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
+
+minDistanceOfArray = determineMinimumOfArray(points)
+maxDistanceOfArray = determineMaximumOfArray(points)
+
+print("Minimum distance of all points = ", str(minDistanceOfArray))
+print("Maximum distance of all points = ", str(maxDistanceOfArray))
