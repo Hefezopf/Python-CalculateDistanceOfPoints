b3d0fa6bc13171ddceee351098401caa75ecbeb7
Hefezopf <markus@hopf-it.de>
2020-06-05 12:22:01 +0200

    determineDistance

--- apptest.py
+++ apptest.py
@@ -2,12 +2,22 @@ import unittest
 from pointmodule import Point
 from pointutilmodule import PointUtil
 
+
 class TestApp(unittest.TestCase):
 
     def test_Point(self):
         p = Point(1, 2)
         self.assertEqual(p.x, 1)
         self.assertEqual(p.y, 2)
+
+    def test_PointUtil_determineDistance1(self):
+        val = PointUtil().determineDistance(Point(1, 1), Point(2, 2))
+        self.assertEqual(val, 1.4142135623730951)
+
+    def test_PointUtil_determineDistance2(self):
+        val = PointUtil().determineDistance(Point(1, 1), Point(3, 1))
+        self.assertEqual(val, 2.0)
+
 
     def test_PointUtil_determineMinimumOfArray(self):
         val = PointUtil().determineMinimumOfArray(
--- pointutilmodule.py
+++ pointutilmodule.py
@@ -1,3 +1,4 @@
+import math
 from pointmodule import Point
 
 
@@ -6,7 +7,29 @@ class PointUtil:
     def parseAllCoords(self, args):
         return [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
 
+
+        # public static double determineMaximumOfArray(Point[] points) {
+        # 	if (points.length < 1) {
+        # 		throw new IllegalArgumentException("too less points");
+        # 	}
+        # 	double max = 0;
+        # 	for (int i = 0; i < points.length; i++) {
+        # 		Point startPoint = points[i];
+        # 		if (startPoint == null) {
+        # 			throw new IllegalArgumentException("startPoint is null");
+        # 		}
+        # 		max = calculateMaximum(points, startPoint, max, i);
+        # 	}
+
+        # 	return limitValue(max, 2);
+        # }
+
+    def determineDistance(self, p1, p2):
+        return math.sqrt(math.pow((p2.x - p1.x), 2) + math.pow((p2.y - p1.y), 2))
+
     def determineMinimumOfArray(self, points):
+        # for x in points:
+        #    print("We're on time %d" % (x.x))
         return 2.0
 
     def determineMaximumOfArray(self, points):
