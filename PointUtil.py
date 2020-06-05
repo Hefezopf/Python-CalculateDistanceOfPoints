76343cb8d9cde9b7fcefac39e53674580d8f25df
Hefezopf <markus@hopf-it.de>
2020-06-05 11:14:56 +0200

    max

---
+++ PointUtil.py
@@ -0,0 +1,12 @@
+from Point import Point
+
+class PointUtil:
+
+    def parseAllCoords(self, args):
+        return [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
+
+    def determineMinimumOfArray(self, points):
+        return 2.0
+
+    def determineMaximumOfArray(self, points):
+        return 5.39
--- app.py
+++ app.py
@@ -2,27 +2,17 @@
 
 import sys
 from Point import Point
-
-def parseAllCoords(args):
-   return [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
-
-def determineMinimumOfArray(points):
-   return 2.0
-
-def determineMaximumOfArray(points):
-   return 5.39
-
-
+from PointUtil import PointUtil
 
 print("Start App...")
 
 if len(sys.argv) > 1:
-  points = parseAllCoords(sys.argv)
+  points = PointUtil().parseAllCoords(sys.argv)
 else:
   points = [Point(1, 1), Point(1, 3), Point(4, 4), Point(6, 1)]
 
-minDistanceOfArray = determineMinimumOfArray(points)
-maxDistanceOfArray = determineMaximumOfArray(points)
+minDistanceOfArray = PointUtil().determineMinimumOfArray(points)
+maxDistanceOfArray = PointUtil().determineMaximumOfArray(points)
 
 print("Minimum distance of all points = ", str(minDistanceOfArray))
 print("Maximum distance of all points = ", str(maxDistanceOfArray))
