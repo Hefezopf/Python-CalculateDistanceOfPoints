7b401f35fd761b4867e42fb4700385b0f96593b9
Hefezopf <markus@hopf-it.de>
2020-06-18 16:48:31 +0200

    del numpy

--- app.py
+++ app.py
@@ -3,10 +3,6 @@
 import sys
 from pointmodule import Point
 from pointutilmodule import PointUtil
-import numpy as np
-
-x = np.arange(15, dtype=np.int64).reshape(3, 5)
-x[1:, ::2] = -99
 
 print("Start App...")
 
